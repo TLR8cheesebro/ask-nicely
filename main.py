81fa0032985c05ec264c7c1c6e85fb8857f488fb
TLR8cheesebro <theo.victoria9@gmail.com>
2025-08-13 18:33:24 -0700

    AI prompts with tokens before testing

--- main.py
+++ main.py
@@ -1,11 +1,23 @@
 import os
 from dotenv import load_dotenv
 
+from google import genai
+
+client = genai.Client(api_key=api_key)
+
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
 
 def main():
     print("Hello from ask-nicely!")
 
+    response = client.models.generate_content(
+        model='gemini-2.0-flash-001', contents='Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.'
+    )
+
+    print(f"Response: {response.text}")
+    print(f"Prompt tokens: {prompt_token_count}")
+    print(f"Response tokens: {candidate_token_count}")
+    
 if __name__ == "__main__":
     main()
