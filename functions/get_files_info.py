bebee72cecb7aca75381319651aa8b064bf7f0a7
TLR8cheesebro <theo.victoria9@gmail.com>
2025-08-25 22:05:11 -0400

    D1. starting todays work 08-25-2025

---
+++ functions/get_files_info.py
@@ -0,0 +1,8 @@
+# limit ai to these things
+import os 
+import sys
+
+def get_files_info(working_directory, directory="."):
+    log_text = print(f"{os.path.join(working_directory, directory)}")
+
+    return log_text
