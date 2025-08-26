6d0b8e1bde6992118f0e231c08d18db0e6774788
TLR8cheesebro <theo.victoria9@gmail.com>
2025-08-26 00:22:42 -0400

    made some progress on the get_files_info function

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -2,6 +2,13 @@
 import os 
 import sys
 
+working_directory = os.getcwd()
+
+
+print("hello from get files.py")
+print(f"checking path; {working_directory}")
+
+
 def get_files_info(working_directory, directory="."):
     log_text = print(f"{os.path.join(working_directory, directory)}")
 
