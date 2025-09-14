a38914da34a2709709a43f021525855cef9d6fa0
TLR8cheesebro <theo.victoria9@gmail.com>
2025-09-14 09:47:54 -0400

    trying to understand abspath

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -4,12 +4,14 @@ import sys
 
 working_directory = os.getcwd()
 
-
-print("hello from get files.py")
+print("hello from get files info.py")
 print(f"checking path; {working_directory}")
 
-
 def get_files_info(working_directory, directory="."):
-    log_text = print(f"{os.path.join(working_directory, directory)}")
-
+    log_text = print(f"{os.path.abspath(directory)}")
+    
     return log_text
+
+
+if __name__ = "get_files_info":
+    get_files_info(working_directory, directory=".")
