dc86f02f0da7e54f737a7ea65e0a2ac0144ffc1c
TLR8cheesebro <theo.victoria9@gmail.com>
2025-09-14 10:25:46 -0400

    i think this should work

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -11,12 +11,10 @@ print(f"this is current directory: {os.getcwd()}")
 print("lets get started . . . ")
 
 def get_files_info(working_directory, directory="."):
-    abs_directory = print(f"{os.path.abspath(directory)}")
+    abs_directory = os.path.abspath(directory)
     
-    if abs_directory not in working_directory:
+    if working_directory not in abs_directory:
         print(f'Error: Cannot list "{directory}" is outside permitted space.')
         return []
 
-
-if __name__ == "__get_files_info__":
-    get_files_info(working_directory, directory=".")
+get_files_info(working_directory)
