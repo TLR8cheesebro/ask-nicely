e876a0b834d2cbd3f0f5ce8a99d84892e583c09a
TLR8cheesebro <theo.victoria9@gmail.com>
2025-09-14 09:51:27 -0400

    fixed github merge error

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -3,17 +3,6 @@ import os
 import sys
 
 working_directory = os.getcwd()
-<<<<<<< HEAD
-=======
-directory = 
-
-print("hello from get files.py")
-print(f"checking path; {working_directory}")
-print(f"checking directory; {directory}")
-
-def get_files_info(working_directory, directory="."):
-    log_text = print(f"{os.path.join(working_directory, directory)}")
->>>>>>> 1e009f2ebc7a10b09ff993295a3b616dc16cd3ca
 
 print("hello from get files info.py")
 print(f"checking path; {working_directory}")
@@ -24,5 +13,5 @@ def get_files_info(working_directory, directory="."):
     return log_text
 
 
-if __name__ = "get_files_info":
+if __name__ == "get_files_info":
     get_files_info(working_directory, directory=".")
