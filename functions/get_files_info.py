8595728b7c2048502fa5c94174325b0967753c6d
TLR8cheesebro <theo.victoria9@gmail.com>
2025-09-14 09:54:27 -0400

    more debug printf

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -7,10 +7,14 @@ working_directory = os.getcwd()
 print("hello from get files info.py")
 print(f"checking path; {working_directory}")
 
+directory = os.path.abspath(".")
+
+print(f"absolute path: {directory}")
+
 def get_files_info(working_directory, directory="."):
     log_text = print(f"{os.path.abspath(directory)}")
     
-    return log_text
+    print(log_text)
 
 
 if __name__ == "get_files_info":
